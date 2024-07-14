8c68a5562a21bb9b2bb1bacebe9d669f0023b6eb
nemunemu <nemunemu4426@gmail.com>
2024-07-14 10:31:52 +0900

    first commit

---
+++ car_manager.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.cars = []
+        self.speed = STARTING_MOVE_DISTANCE
+        self.generate_car()
+
+    def generate_car(self):
+        x = 300
+        y = random.randint(-240, 240)
+        self.add_car((x, y))
+
+    def add_car(self, position):
+        car = Turtle(shape="square")
+        car.color(random.choice(COLORS))
+        car.shapesize(1, 2)
+        car.penup()
+        car.goto(position)
+        car.setheading(180)
+        self.cars.append(car)
+
+    def move(self):
+        for car in self.cars:
+            car.forward(self.speed)
+            if car.xcor() <= -310:
+                car.hideturtle()
+                car.clear()
+
+        self.cars = [car for car in self.cars if car.xcor() > -310]
+    
+    def increase_speed(self):
+        self.speed += MOVE_INCREMENT
+
+    def detect_collision_with_player(self, player):
+        for car in self.cars:
+            if car.distance(player) < 20:
+                return True
+        return False
+
---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
+game_is_on = True
+count = 0
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.move()
+    if count % 10 == 0:
+        car_manager.generate_car()
+
+    if player.ycor() > 280:
+        player.refresh()
+        scoreboard.increase_level()
+        car_manager.increase_speed()
+    
+    if car_manager.detect_collision_with_player(player):
+        scoreboard.game_over()
+        game_is_on = False
+    
+    count += 1
+
+screen.exitonclick()
